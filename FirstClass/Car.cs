105deb8a5a4546561b7374d126cb03ffea06e815
David Reina <davidereina@hotmail.com>
2023-10-05 16:04:59 -0500

    Simple program that creates a Car class instance called 'aveo' and use its methods Drive(), Details() and Stop()

---
+++ FirstClass/Car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstClass
+{
+    internal class Car
+    {
+        private string _name;
+        private int _hp;
+        private string _color;
+        public Car(string name, int hp, string color = "black") 
+        {
+            _name = name;
+            _hp = hp;
+            _color = color;
+            Console.WriteLine($"{name} was created");
+        }
+
+        public void Drive()
+        {
+            Console.WriteLine(_name + " is driving");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine($"{_name} stopped!");
+        }
+
+        public void Details()
+        {
+            Console.WriteLine($"Your {_color} car is {_name} and has {_hp} HP");
+        }
+    }
+}
---
+++ FirstClass/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace FirstClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car aveo = new Car("Chevrolet Aveo", 400, "white");
+            aveo.Drive();
+            aveo.Details();
+
+            Console.WriteLine("Press 1 to stop the car: ");
+            string userInput = Console.ReadLine();
+            if(userInput.Equals("1"))
+            {
+                aveo.Stop();
+            }
+            else
+            {
+                Console.WriteLine("Car is still driving");
+            }
+        }
+    }
+}
